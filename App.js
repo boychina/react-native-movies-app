54913cdf31e0c67f8f62e41e763b08217770f45f
boychina <boychina@outlook.com>
2018-11-15 21:50:34 +0800

    feat(INIT):Image

--- App.js
+++ App.js
@@ -7,23 +7,24 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Image, StyleSheet, Text, View } from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const MOCKED_MOVIES_DATA = [
+  {
+    title: "标题",
+    year: "2015",
+    posters: { thumbnail: "http://i.imgur.com/UePbdph.jpg" }
+  }
+];
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
+    const movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image source={{ uri: movie.posters.thumbnail }} style={styles.thumbnail} />
       </View>
     );
   }
@@ -32,18 +33,12 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  }
 });
