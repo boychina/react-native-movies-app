92c3376c6e33a11f7c7266376ff4708d11a3602a
Mr厉害 <zhaohuanplus@163.com>
2018-11-17 01:03:34 +0800

    feat(Base): test SearchBar

--- App.js
+++ App.js
@@ -475,89 +475,124 @@
 
 
 // tslint:disable:jsx-no-multiline-js
+// import React from 'react';
+// import { ScrollView, StyleSheet, Text, View } from 'react-native';
+// import { Button, Drawer, List, WhiteSpace } from 'antd-mobile-rn';
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+
+// export default class DrawerExample extends React.Component {
+//   drawer;
+
+//   onOpenChange = (isOpen) => {
+//     /* tslint:disable: no-console */
+//     console.log('是否打开了 Drawer', isOpen.toString());
+//   }
+
+//   render() {
+//     const itemArr = Array.apply(null, Array(20))
+//       .map(function(_, i) {
+//         return i;
+//       })
+//       .map((_i, index) => {
+//         if (index === 0) {
+//           return (
+//             <List.Item
+//               key={index}
+//               thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+//               multipleLine
+//             >
+//               <View
+//                 style={{
+//                   flexDirection: 'row',
+//                   justifyContent: 'space-between',
+//                   alignItems: 'center',
+//                 }}
+//               >
+//                 <Text>Categories - {index}</Text>
+//                 <Button
+//                   type="primary"
+//                   size="small"
+//                   onPress={() => this.drawer.closeDrawer()}
+//                 >
+//                   close drawer
+//                 </Button>
+//               </View>
+//             </List.Item>
+//           );
+//         }
+//         return (
+//           <List.Item
+//             key={index}
+//             thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
+//           >
+//             <Text>Categories - {index}</Text>
+//           </List.Item>
+//         );
+//       });
+
+//     // Todo: https://github.com/DefinitelyTyped/DefinitelyTyped
+//     const sidebar = (
+//       <ScrollView style={[styles.container]}>
+//         <List>{itemArr}</List>
+//       </ScrollView>
+//     );
+
+//     return (
+//       <Drawer
+//         sidebar={sidebar}
+//         position="left"
+//         open={false}
+//         drawerRef={(el) => (this.drawer = el)}
+//         onOpenChange={this.onOpenChange}
+//         drawerBackgroundColor="#ccc"
+//       >
+//         <View style={{ flex: 1, marginTop: 114, padding: 8 }}>
+//           <Button onPress={() => this.drawer && this.drawer.openDrawer()}>
+//             Open drawer
+//           </Button>
+//           <WhiteSpace />
+//         </View>
+//       </Drawer>
+//     );
+//   }
+// }
+
+
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Button, Drawer, List, WhiteSpace } from 'antd-mobile-rn';
+import { Alert, View } from 'react-native';
+import { SearchBar } from 'antd-mobile-rn';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+export default class SearchBarDemo extends React.Component<any, any> {
+  state = {
+    value: '美食',
+  };
 
-export default class DrawerExample extends React.Component {
-  drawer;
+  onChange = (value: any) => {
+    this.setState({ value });
+  }
 
-  onOpenChange = (isOpen) => {
-    /* tslint:disable: no-console */
-    console.log('是否打开了 Drawer', isOpen.toString());
+  clear = () => {
+    this.setState({ value: '' });
   }
 
   render() {
-    const itemArr = Array.apply(null, Array(20))
-      .map(function(_, i) {
-        return i;
-      })
-      .map((_i, index) => {
-        if (index === 0) {
-          return (
-            <List.Item
-              key={index}
-              thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-              multipleLine
-            >
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                }}
-              >
-                <Text>Categories - {index}</Text>
-                <Button
-                  type="primary"
-                  size="small"
-                  onPress={() => this.drawer.closeDrawer()}
-                >
-                  close drawer
-                </Button>
-              </View>
-            </List.Item>
-          );
-        }
-        return (
-          <List.Item
-            key={index}
-            thumb="https://zos.alipayobjects.com/rmsportal/eOZidTabPoEbPeU.png"
-          >
-            <Text>Categories - {index}</Text>
-          </List.Item>
-        );
-      });
-
-    // Todo: https://github.com/DefinitelyTyped/DefinitelyTyped
-    const sidebar = (
-      <ScrollView style={[styles.container]}>
-        <List>{itemArr}</List>
-      </ScrollView>
-    );
-
     return (
-      <Drawer
-        sidebar={sidebar}
-        position="left"
-        open={false}
-        drawerRef={(el) => (this.drawer = el)}
-        onOpenChange={this.onOpenChange}
-        drawerBackgroundColor="#ccc"
-      >
-        <View style={{ flex: 1, marginTop: 114, padding: 8 }}>
-          <Button onPress={() => this.drawer && this.drawer.openDrawer()}>
-            Open drawer
-          </Button>
-          <WhiteSpace />
-        </View>
-      </Drawer>
+      <View style={{ marginTop: 30 }}>
+        <SearchBar defaultValue="初始值" placeholder="搜索" />
+        <SearchBar
+          value={this.state.value}
+          placeholder="搜索"
+          onSubmit={(value: any) => Alert.alert(value)}
+          onCancel={this.clear}
+          onChange={this.onChange}
+          showCancelButton
+        />
+      </View>
     );
   }
 }
