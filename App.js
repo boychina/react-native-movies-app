5e1498b3dd46baad59c3c009e8ad09129608bb97
boychina <boychina@outlook.com>
2018-11-17 16:08:59 +0800

    feat(Base):SearchBar

--- App.js
+++ App.js
@@ -567,12 +567,12 @@ import React from 'react';
 import { Alert, View } from 'react-native';
 import { SearchBar } from 'antd-mobile-rn';
 
-export default class SearchBarDemo extends React.Component<any, any> {
+export default class SearchBarDemo extends React.Component {
   state = {
     value: '美食',
   };
 
-  onChange = (value: any) => {
+  onChange = (value) => {
     this.setState({ value });
   }
 
@@ -582,12 +582,12 @@ export default class SearchBarDemo extends React.Component<any, any> {
 
   render() {
     return (
-      <View style={{ marginTop: 30 }}>
+      <View style={{ marginTop: 80 }}>
         <SearchBar defaultValue="初始值" placeholder="搜索" />
         <SearchBar
           value={this.state.value}
           placeholder="搜索"
-          onSubmit={(value: any) => Alert.alert(value)}
+          onSubmit={(value) => Alert.alert(value)}
           onCancel={this.clear}
           onChange={this.onChange}
           showCancelButton
