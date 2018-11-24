bcbf4034ff333bf3c4150271d47ffeeed8ad436b
boychina <boychina@outlook.com>
2018-11-25 00:52:27 +0800

    feat(Base): 大的

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import Root from './src/Root';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Root);
---
+++ src/Root.js
@@ -0,0 +1,20 @@
+import React, {
+  Component
+} from 'react';
+import {
+  Provider
+} from 'react-redux';
+import configureStore from './store/ConfigureStore';
+import App from './container/App'; // app的入口
+
+const store = configureStore();
+
+export default class Root extends Component {
+  render() {
+    return (
+      <Provider store = {store} >
+        <App />
+      </Provider>
+    )
+  }
+}
