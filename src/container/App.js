25728298529ba400f82cee6e5b67a245de7df197
boychina <boychina@outlook.com>
2018-12-07 12:56:20 +0800

    feat(Base): 项目结构调整

--- src/container/App.js
+++ src/container/App.js
@@ -2,9 +2,9 @@ import { StackNavigator } from 'react-navigation';
 // import LoginPage from '../pages/LoginPage'
 // import MainPage from '../pages/MainPage'
 
-import HomePage from '../pages/Home';
-import MoviesPage from '../pages/Movies';
-import MoviesMore from '../pages/MoviesMore';
+import HomePage from './Home';
+import MoviesPage from './Movies';
+import MoviesMore from './MoviesMore';
 
 const App = StackNavigator({
   Home: {
