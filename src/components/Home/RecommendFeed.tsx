56d68e155e2124ed4b651ae5f1b94a5140661eeb
boychina <boychina@outlook.com>
2018-12-13 13:55:46 +0800

    feat(typescript): 动态typescript

--- src/components/Home/RecommendFeed.tsx
+++ src/components/Home/RecommendFeed.tsx
@@ -6,7 +6,7 @@ export default class RecommendFeed extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <Text>Hello Wrold!</Text>
       </View>
     );
   }
